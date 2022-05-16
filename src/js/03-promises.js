3147173117a785b92b819ccaefe1eba62cbcbb23
Serhii <89203890+zhchok@users.noreply.github.com>
2022-05-16 23:06:59 +0300

    finish

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,8 +6,9 @@ const refs = {
 	inputAmount: document.querySelector('[name="amount"]'),
 	form: document.querySelector(".form"),
 };
+let counter = 0;
 
-let counter = 1;
+refs.form.addEventListener("submit", handleSubmit);
 
 function createPromise(position, delay) {
 	return new Promise((resolve, reject) => {
@@ -22,16 +23,23 @@ function createPromise(position, delay) {
 	});
 }
 
-function createPromises() {
-	const amount = Number(refs.inputAmount.value);
+function convertInputValueToNumber() {
 	const delay = Number(refs.inputDelay.value);
 	const step = Number(refs.inputDealeyStep.value);
+	const amount = Number(refs.inputAmount.value);
+	return { delay, step, amount };
+}
+
+function createPromises() {
+	const { delay, step, amount } = convertInputValueToNumber();
 
 	for (let i = 0; i < amount; i += 1) {
-		const position = counter;
+		const totalDelay = delay + step * i;
 		counter += 1;
+		const position = counter;
+
 		setTimeout(() => {
-			createPromise(position, delay + step * i)
+			createPromise(position, totalDelay)
 				.then(({ position, delay }) => {
 					Notify.success(`Fulfilled promise ${position} in ${delay} ms`);
 				})
@@ -41,7 +49,7 @@ function createPromises() {
 			if (position === amount) {
 				counter = 1;
 			}
-		}, delay + step * i);
+		}, totalDelay);
 	}
 }
 
@@ -49,5 +57,3 @@ function handleSubmit(e) {
 	e.preventDefault();
 	createPromises();
 }
-
-refs.form.addEventListener("submit", handleSubmit);
